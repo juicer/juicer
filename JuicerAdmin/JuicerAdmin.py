684a6284d70d8077dc437f948105c687a1d9594f
Andrew Butcher <abutcher@redhat.com>
2012-06-13 23:51:53 -0400

    Working over http with python-requests library.

--- JuicerAdmin/JuicerAdmin.py
+++ JuicerAdmin/JuicerAdmin.py
@@ -1,40 +1,35 @@
 # -*- coding: utf-8 -*-
 import JuicerAdmin
-import httplib2
-import urllib
+import json
+import requests
 
 class JuicerAdmin(object):
     def __init__(self, args):
         self.args = args
-        self._http = httplib2.Http(disable_ssl_certificate_validation=True)
         self.envs = self.args.envs
 
         # Some default shit, read this in from yaml in util.py
-        connect_params = {'user': 'admin', 
-                          'password': 'admin',
-                          'base_url': 'https://snarl.rdu.redhat.com/pulp/api'}
+        connect_params={'user': 'admin',
+                        'password': 'admin',
+                        'base_url': 'http://snarl.rdu.redhat.com/pulp/api'}
 
-        self._http.add_credentials(connect_params['user'], connect_params['password'])
-        self.base_url = connect_params['base_url']
-
-    def read(self, url="", method='GET', params={}, headers={}):
+        self.base_url=connect_params['base_url']
+        self.auth=(connect_params['user'], connect_params['password'])
+        self.headers={'content-type': 'application/json'}
+        
+    def put(self, url="", data={}):
         # Returns (response, content)
-        return self._http.request(url, method, urllib.urlencode(params), headers)
-        
-    def create_repo(self):
-        params = { 'arch': 'noarch',
-                   'feed': '',
-                   'publish': True,
-                   'name': 'derp-test' }
+        return requests.put(url, json.dumps(data), auth=self.auth, headers=self.headers)
+    
+    def get(self, url=""):
+        # Returns (response, content)
+        return requests.get(url, self.auth, self.headers)
 
-        method = 'POST'
-        query = '/repositories/'
-        url = self.base_url + query
-        headers = {'Content-type': 'application/x-www-form-urlencoded'} 
-        
+    def create_repo(self, query='/repositories/'):
+        data = { 'name': self.args.name,
+                 'arch': 'noarch'}
         for env in self.envs:
-            params['id'] = '-'.join([params['name'], env])
-            params['relative_path'] = "/%s/%s/" % (env, params['name'])
-            (response, content) = self.read(url, method, params, headers)
-            print response
-            print content
+            data['relative_path']='//%s/%s/' % (env, self.args.name)
+            data['id']='-'.join([self.args.name, env])
+            url = self.base_url + query
+            r = self.put(url, data)
