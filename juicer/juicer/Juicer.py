e42b4ea3cea388f88f9cf65dad3150a74a1f78e1
Alex Chvatal <achvatal@redhat.com>
2012-06-29 17:05:21 -0400

    basic upload functionality for individual files

--- juicer/juicer/Juicer.py
+++ juicer/juicer/Juicer.py
@@ -19,6 +19,9 @@ from juicer.common import Constants
 import juicer.juicer
 import juicer.utils
 import re
+import os
+import rpm
+import hashlib
 
 
 class Juicer(object):
@@ -26,6 +29,98 @@ class Juicer(object):
         self.args = args
 
         self.connectors = juicer.utils.get_login_info()
+
+    # starts the 3-step upload process
+    def _init_up(self, query='/services/upload/', name='', cksum='', size='', \
+            env='re'):
+        data = {'name':name,
+                'checksum':cksum,
+                'size':size}
+
+        _r = self.connectors[env].post(query, data)
+        uid = juicer.utils.load_json_str(_r.content)['id']
+
+        return uid
+ 
+    # continues 3-step upload process. this is where actual data transfer
+    # occurs!
+    def _append_up(self, query='/services/upload/append/', uid='', fdata='', env='re'):
+        uri = query + uid + '/'
+        data = {'file-id':uid,
+                'file-data':fdata}
+
+        _r = self.connectors[env].put(uri, data)
+
+        return juicer.utils.load_json_str(_r.content)
+
+    # finalizes the 3-step upload process. this is where metadata is set
+    def _import_up(self, query='/services/upload/import/', uid='', name='', ftype='rpm', \
+            cksum='', htype='md5', nvrea='', size='', lic='', group='', vendor='', \
+            req='', env='re'):
+        data = {'uploadid':uid,
+                'metadata':{
+                    'type':ftype,
+                    'checksum':cksum,
+                    'hashtype':htype,
+                    'pkgname':name,
+                    'nvrea':nvrea,
+                    'size':size,
+                    'license':lic,
+                    'group':group,
+                    'vendor':vendor,
+                    'requires':req}}
+
+        _r = self.connectors[env].post(query, data)
+
+        return juicer.utils.load_json_str(_r.content)
+
+    # this is used to upload files to pulp
+    def upload(self, items=[], repos=[], envs=[], output=[]):
+        if envs == None:
+            envs = ['re']
+
+        ts = rpm.TransactionSet()
+
+        for item in items:
+            if os.path.isfile(item):
+                # process individual file
+                rpm_fd = open(item)
+                pkg = ts.hdrFromFdno(rpm_fd)
+                name = pkg['name']
+                version = pkg['version']
+                release = pkg['release']
+                epoch = 0
+                arch = pkg['arch']
+                nvrea = tuple((name, version, release, epoch, arch))
+                cksum = hashlib.md5(item).hexdigest()
+                size = os.path.getsize(item)
+
+                # initiate uploade
+                upload_id = self._init_up(name=name, cksum=cksum, size=size)
+
+                # read in rpm
+                rpm_data = unicode(rpm_fd.readlines())
+                upload_flag = self._append_up(uid=upload_id, fdata=rpm_data)
+
+                # finalize upload
+                if upload_flag == True:
+                    code = self._import_up(uid=upload_id, name=name, cksum=cksum, \
+                        nvrea=nvrea, size=size)
+
+                rpm_fd.close()
+
+            elif os.path.isdir(item):
+                # process dir
+                output.append('dir')
+            elif re.match('https?://.*', item):
+                # download item and upload
+                output.append('url')
+            else:
+                # TODO raise an actual error
+                output.append('No! Bad!')
+                break
+
+        return output
 
     def search_cart(self, query='/services/search/cart', output=[]):
         pass
