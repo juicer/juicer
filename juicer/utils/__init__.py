d5fb6f4c1a42babb3c30955f78036684cbd162cf
Andrew Butcher <abutcher@redhat.com>
2012-07-14 17:19:47 -0400

    Revert "Thought I removed this before."
    Reverting this in favor of replacing base with DEFAULT which according to tbielawa is removed via the configparser library.
    
    This relates to #1.
    
    This reverts commit b5160653369c5376083540940c768cf07d55893b.

--- juicer/utils/__init__.py
+++ juicer/utils/__init__.py
@@ -100,7 +100,6 @@ def get_environments():
     juicer.utils.Log.log_debug("Reading environment sections:")
 
     environments = config.sections()
-    environments.remove('base')
     return environments
 
 
