f22e3e61dd719d56ace18ce37e022055be2ee7d0
Andrew Butcher <abutcher@redhat.com>
2012-07-24 14:01:18 -0400

    Make mute more transparent for #46

--- juicer/tests/TestJuicerAdmin.py
+++ juicer/tests/TestJuicerAdmin.py
@@ -51,7 +51,7 @@ class TestJuicerAdmin(unittest.TestCase):
         self.create_test_repo()
         args = self.parser.parser.parse_args('show-repo test-repo-456'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.show_repo)(name=args.name, envs=args.envs)
+        output = mute(returns_output=True)(pulp.show_repo)(name=args.name, envs=args.envs)
         self.assertTrue(any('test-repo-456' in k for k in output))
         self.delete_test_repo()
 
@@ -59,28 +59,28 @@ class TestJuicerAdmin(unittest.TestCase):
         self.create_test_repo()
         args = self.parser.parser.parse_args('delete-repo test-repo-456'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.delete_repo)(name=args.name, envs=args.envs)
+        output = mute(returns_output=True)(pulp.delete_repo)(name=args.name, envs=args.envs)
         self.assertTrue(any('deleted' in k for k in output))
 
     def test_list_repos(self):
         self.create_test_repo()
         args = self.parser.parser.parse_args('list-repos'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.list_repos)(envs=args.envs)
+        output = mute(returns_output=True)(pulp.list_repos)(envs=args.envs)
         self.assertTrue('test-repo-456' in output)
         self.delete_test_repo()
 
     def test_create_repo(self):
         args = self.parser.parser.parse_args('create-repo test-repo-456'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.create_repo)(arch=args.arch, name=args.name, envs=args.envs)
+        output = mute(returns_output=True)(pulp.create_repo)(arch=args.arch, name=args.name, envs=args.envs)
         self.assertTrue(any('created' in k for k in output))
         self.delete_test_repo()
 
     def test_create_user(self):
         args = self.parser.parser.parse_args('create-user cjesop --password cjesop --name "ColonelJesop"'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.create_user)(login=args.login, name=args.name, \
+        output = mute(returns_output=True)(pulp.create_user)(login=args.login, name=args.name, \
                                                   password=args.password, envs=args.envs)
         self.assertTrue(any('created' in k for k in output))
         self.delete_test_user()
@@ -89,21 +89,21 @@ class TestJuicerAdmin(unittest.TestCase):
         self.create_test_user()
         args = self.parser.parser.parse_args('delete-user cjesop'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.delete_user)(login=args.login, envs=args.envs)
+        output = mute(returns_output=True)(pulp.delete_user)(login=args.login, envs=args.envs)
         self.assertTrue(any('deleted' in k for k in output))
 
     def test_show_user(self):
         self.create_test_user()
         args = self.parser.parser.parse_args('show-user cjesop'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.show_user)(login=args.login, envs=args.envs)
+        output = mute(returns_output=True)(pulp.show_user)(login=args.login, envs=args.envs)
         self.assertTrue(any('cjesop' in k for k in output))
         self.delete_test_user()
 
     def test_list_roles(self):
         args = self.parser.parser.parse_args('list-roles'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.list_roles)(envs=args.envs)
+        output = mute(returns_output=True)(pulp.list_roles)(envs=args.envs)
         self.assertTrue(any('super-users' in k for k in output))
 
     def test_role_add(self):
@@ -111,7 +111,7 @@ class TestJuicerAdmin(unittest.TestCase):
         args = self.parser.parser.parse_args('role-add --login cjesop \
                 --role super-users'.split())
         pulp = ja(args)
-        output = mute(True)(pulp.role_add)(role=args.role, login=args.login, envs=args.envs)
+        output = mute(returns_output=True)(pulp.role_add)(role=args.role, login=args.login, envs=args.envs)
         self.assertTrue(any('added' in k for k in output))
         self.delete_test_user()
 
