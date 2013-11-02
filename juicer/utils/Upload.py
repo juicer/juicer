24bc840dd4e125f97b0475ce3339654cede2ed45
Andrew Butcher <abutcher@redhat.com>
2013-11-02 19:42:19 -0400

    Clean up some cruft.

--- juicer/utils/Upload.py
+++ juicer/utils/Upload.py
@@ -96,8 +96,6 @@ class Upload(object):
         _r = self.connector.post(query, data)
 
         if _r.status_code not in [Constants.PULP_POST_OK, Constants.PULP_POST_ACCEPTED]:
-            print _r.status_code
-            print _r.content
             juicer.utils.Log.log_error("Import error importing '%s'... server said: \n %s", (self.pkg_name,
                                        juicer.utils.load_json_str(_r.content)))
             _r.raise_for_status()
